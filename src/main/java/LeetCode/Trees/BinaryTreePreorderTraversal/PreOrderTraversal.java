fd5f2d01c4bbf129a07851783368500b2728a39c
Oindrila Banerjee <banerjee.oindrila.ob@gmail.com>
2023-09-03 23:32:42 +0530

    pre order traversal - trees

--- src/main/java/LeetCode/Trees/BinaryTreePreorderTraversal/PreOrderTraversal.java
+++ src/main/java/LeetCode/Trees/BinaryTreePreorderTraversal/PreOrderTraversal.java
@@ -8,7 +8,15 @@ import java.util.List;
 public class PreOrderTraversal {
     List<Integer> treeElements = new ArrayList<>();
     public List<Integer> preorderTraversal(TreeNode root) {
-
+        traverse(root);
         return treeElements;
+    }
+
+    private void traverse(TreeNode root) {
+        if (root == null) return;;
+
+        treeElements.add(root.val);
+        traverse(root.left);
+        traverse(root.right);
     }
 }
