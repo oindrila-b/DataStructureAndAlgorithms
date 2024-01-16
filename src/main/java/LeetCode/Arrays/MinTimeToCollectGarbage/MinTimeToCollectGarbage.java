64319664901853552ca8ffa45a6f6c0e9c233506
oindrilabanerjee <banerjee.oindrila.ob@gmail.com>
2024-01-16 17:38:54 +0530

    Leetcode Answers

---
+++ src/main/java/LeetCode/Arrays/MinTimeToCollectGarbage/MinTimeToCollectGarbage.java
@@ -0,0 +1,16 @@
+package main.java.LeetCode.Arrays.MinTimeToCollectGarbage;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MinTimeToCollectGarbage {
+    public int garbageCollection(String[] garbage, int[] travel) {
+
+        Map<Character, Integer> garbageTypeTimeMap = new HashMap<>(Map.of('M', 1 , 'P', 1, 'G', 1));
+        for(String s : garbage) {
+            int truckPosition = 0;
+            return  -1;
+        }
+        return  -1;
+    }
+}
