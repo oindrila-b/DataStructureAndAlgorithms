8417848d9d733f9f73e7fe6e06792428c704a4dd
Oindrila Banerjee <banerjee.oindrila.ob@gmail.com>
2024-01-18 21:22:10 +0530

    Arrays

--- src/main/java/LeetCode/TwoPointer/ThreeSumClosest/ThreeSumClosest.java
+++ src/main/java/LeetCode/TwoPointer/ThreeSumClosest/ThreeSumClosest.java
@@ -1,7 +1,7 @@
-//package main.java.LeetCode.TwoPointer.ThreeSumClosest;
-//
-//public class ThreeSumClosest {
-//    public int threeSumClosest(int[] nums, int target) {
-//
-//    }
-//}
+package main.java.LeetCode.TwoPointer.ThreeSumClosest;
+
+public class ThreeSumClosest {
+    public int threeSumClosest(int[] nums, int target) {
+        return -1;
+    }
+}
