d17c229cf5630db47574ae33d3dcd804f3dac0d9
oindrilabanerjee <banerjee.oindrila.ob@gmail.com>
2024-02-11 02:59:29 +0530

    Leetcode Answers

--- src/main/java/LeetCode/Trees/BinaryTreeLevelOrderTraversal/BinaryTreeLevelOrderTraversal.java
+++ src/main/java/LeetCode/Trees/BinaryTreeLevelOrderTraversal/BinaryTreeLevelOrderTraversal.java
@@ -2,11 +2,28 @@ package main.java.LeetCode.Trees.BinaryTreeLevelOrderTraversal;
 
 import main.java.LeetCode.Trees.TreeNode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 public class BinaryTreeLevelOrderTraversal {
-
+    public List<List<Integer>> nodeList = new ArrayList<>();
     public List<List<Integer>> levelOrder(TreeNode root) {
-
+        if (root == null) return new ArrayList<>();
+        setNodes(root, 0);
+        return nodeList;
     }
+
+    public void setNodes(TreeNode node, int level) {
+        if (node == null) return;
+        if (nodeList.size() == level) {
+            nodeList.add(level, new ArrayList<>());
+        }
+
+        nodeList.get(level).add(node.val);
+        setNodes(node.left, level+1);
+        setNodes(node.right, level+1);
+    }
+
 }
