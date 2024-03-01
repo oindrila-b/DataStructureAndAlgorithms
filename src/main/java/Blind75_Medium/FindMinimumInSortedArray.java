5b4b32f9199d4553cdba3acc594be80bf716e21f
oindrilabanerjee <banerjee.oindrila.ob@gmail.com>
2024-03-01 23:26:08 +0530

    Leetcode Answers - Blind75

--- src/main/java/Blind75_Medium/FindMinimumInSortedArray.java
+++ src/main/java/Blind75_Medium/FindMinimumInSortedArray.java
@@ -2,4 +2,16 @@ package main.java.Blind75_Medium;
 
 public class FindMinimumInSortedArray {
 
+    public int findMin(int[] nums) {
+        int start = 0, end = nums.length -1;
+        while (start < end) {
+            int mid = (start + end) /2;
+            if (nums[mid] < nums[end]) {
+                end = mid;
+            }else {
+                start = mid +1;
+            }
+        }
+        return nums[start];
+    }
 }
