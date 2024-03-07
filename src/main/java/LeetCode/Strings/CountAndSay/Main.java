45823599f44a96cb5491da251e2876f53e105b87
oindrilabanerjee <banerjee.oindrila.ob@gmail.com>
2024-03-08 00:20:02 +0530

    Leetcode Answers

---
+++ src/main/java/LeetCode/Strings/CountAndSay/Main.java
@@ -0,0 +1,9 @@
+package main.java.LeetCode.Strings.CountAndSay;
+
+public class Main {
+    public static void main(String[] args) {
+        CountAndSay countAndSay = new CountAndSay();
+        System.out.println(countAndSay.countAndSay(5));
+
+    }
+}
