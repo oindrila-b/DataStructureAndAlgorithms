9b823a2678198d93cb54aced432da01429bbd9e2
oindrilabanerjee <banerjee.oindrila.ob@gmail.com>
2024-03-13 00:25:20 +0530

    Leetcode Answers

---
+++ src/main/java/LeetCode/LinkedList/Remove0Sum/RemoveZeroSum.java
@@ -0,0 +1,9 @@
+package main.java.LeetCode.LinkedList.Remove0Sum;
+
+import main.java.LeetCode.LinkedList.ListNode;
+
+public class RemoveZeroSum {
+    public ListNode removeZeroSumSublists(ListNode head) {
+        return new ListNode();
+    }
+}
