f8e9eef788474c1fb315e47b785d5dec2f9320e6
oindrilabanerjee <banerjee.oindrila.ob@gmail.com>
2024-04-11 00:07:45 +0530

    Leetcode Answers

---
+++ src/main/java/LeetCode/Arrays/Concatenation/Concatenation2.java
@@ -0,0 +1,18 @@
+package main.java.LeetCode.Arrays.Concatenation;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Concatenation2 {
+    public int[] getConcatenation(int[] nums) {
+        int[] ans = new int[2* nums.length];
+        int k = 0;
+        for(int i = 0; i < 2; i++) {
+            for (int j :nums) {
+                ans[k++] = j;
+            }
+        }
+        return ans;
+    }
+}
