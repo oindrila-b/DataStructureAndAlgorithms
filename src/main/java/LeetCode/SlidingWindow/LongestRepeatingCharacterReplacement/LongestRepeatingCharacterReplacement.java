f9c5da6bd31e32286eea92ce54c5077f57c35922
oindrilabanerjee <banerjee.oindrila.ob@gmail.com>
2024-06-05 23:23:45 +0530

    Leetcode Answers

--- src/main/java/LeetCode/SlidingWindow/LongestRepeatingCharacterReplacement/LongestRepeatingCharacterReplacement.java
+++ src/main/java/LeetCode/SlidingWindow/LongestRepeatingCharacterReplacement/LongestRepeatingCharacterReplacement.java
@@ -3,6 +3,7 @@ package main.java.LeetCode.SlidingWindow.LongestRepeatingCharacterReplacement;
 public class LongestRepeatingCharacterReplacement {
     public int characterReplacement(String s, int k) {
     char[] stringArray = s.toCharArray();
-
+        System.out.println(stringArray);
+        return  1;
     }
 }
