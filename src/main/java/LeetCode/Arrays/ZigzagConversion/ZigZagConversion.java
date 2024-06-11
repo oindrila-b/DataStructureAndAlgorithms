45aba9d55381b4fd71a9a0bbad7735d6a0a320a1
oindrilabanerjee <banerjee.oindrila.ob@gmail.com>
2024-06-12 01:41:25 +0530

    Leetcode Answers

---
+++ src/main/java/LeetCode/Arrays/ZigzagConversion/ZigZagConversion.java
@@ -0,0 +1,16 @@
+package main.java.LeetCode.Arrays.ZigzagConversion;
+
+public class ZigZagConversion {
+    public String convert(String s, int numRows) {
+        char[] characterArray = s.toCharArray();
+        StringBuilder newString = new StringBuilder();
+        int i=0;
+        int rowCount = 0;
+
+         // P  A  Y  P  A  L  I  S  H  I   R  I  N  G
+        while (i < characterArray.length){
+
+        }
+        return newString.toString();
+    }
+}
